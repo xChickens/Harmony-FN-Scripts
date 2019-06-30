33e239fa3d82ec960e78e9af91ee9cb49f9308f0
Christopher Liu <ssuperkid5@yahoo.com>
2019-06-30 03:39:00 -0700

    Remove "reboot"
    it's useless

--- setup.sh
+++ setup.sh
@@ -12,6 +12,5 @@ curl -LO https://raw.githubusercontent.com/xChickens/Harmony-FN-Scripts/master/s
 curl -LO https://raw.githubusercontent.com/xChickens/Harmony-FN-Scripts/master/rank.sh && chmod u+x rank.sh
 curl -LO https://raw.githubusercontent.com/xChickens/Harmony-FN-Scripts/master/.bash_profile
 curl -LO https://raw.githubusercontent.com/xChickens/Harmony-FN-Scripts/master/.bashrc
-source .bashrc
 curl -LO https://harmony.one/wallet.sh && chmod u+x wallet.sh && ./wallet.sh -d && curl -LO https://harmony.one/node.sh && chmod u+x node.sh && curl -LO https://harmony.one/mystatus.sh && chmod u+x mystatus.sh
 reboot
