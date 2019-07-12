3e59b02ba5924fb6aaee7d2d9a613cc45dde4416
Christopher Liu <cmliu@ucsd.edu>
2019-07-12 14:33:35 -0700

    Update stat.sh with fixes

--- devsetup.sh
+++ devsetup.sh
@@ -1,4 +1,4 @@
-export GOPATH=D:/VSCode/Harmony-Dev
+export GOPATH=$HOME/Harmony-Dev
 export CGO_CFLAGS="-I$GOPATH/src/github.com/harmony-one/bls/include -I$GOPATH/src/github.com/harmony-one/mcl/include -I/usr/local/opt/openssl/include"
 export CGO_LDFLAGS="-L$GOPATH/src/github.com/harmony-one/bls/lib -L/usr/local/opt/openssl/lib"
 export LD_LIBRARY_PATH=$GOPATH/src/github.com/harmony-one/bls/lib:$GOPATH/src/github.com/harmony-one/mcl/lib:/usr/local/opt/openssl/lib
